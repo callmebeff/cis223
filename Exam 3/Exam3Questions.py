0a7d37910e06c7f02920f20f239b57ae97b356df
Jeff <callmebeff@gmail.com>
2024-04-06 16:56:06 -0500

    moved questions to excel for E3

---
+++ Exam 3/Exam3Questions.py
@@ -0,0 +1,16 @@
+# Random question generator
+
+import random
+from openpyxl import load_workbook
+
+num = random.randint(2, 36)
+
+ch = f'A{num}'
+cell = f'C{num}'
+
+
+wb = load_workbook(r"C:\Users\jsche\Git\cis223\Exam 2 Review\Questions.xlsx", data_only=True)
+sh = wb["Questions"]
+
+print(f'\n{sh[ch].value}: {sh[cell].value}\n')
+
