b28f1b2e6e164a92a7632d5c2467333ac29080ef
Jeff <callmebeff@gmail.com>
2024-04-13 11:37:33 -0500

    ch11 init

---
+++ Chapter 11 - Binary Search Tree/R11.20.py
@@ -0,0 +1,7 @@
+"""
+Consider the set of keys K = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}.
+a. Draw a (2,4) tree storing K as its keys using the fewest number of
+nodes.
+b. Draw a (2,4) tree storing K as its keys using the maximum number
+of nodes.
+"""
